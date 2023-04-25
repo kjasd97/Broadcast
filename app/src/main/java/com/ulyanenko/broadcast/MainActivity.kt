1fc7015a76f83a3c2f04bc58afd46451796b354c
Roman Ulyanenko <ulyanenkorr@gmail.com>
2023-04-25 10:48:17 +0300

    Initial commit

---
+++ app/src/main/java/com/ulyanenko/broadcast/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.ulyanenko.broadcast
+
+import android.content.Intent
+import android.content.IntentFilter
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    val receiver = MyReceiver()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val intentFilter = IntentFilter ().apply {
+            addAction(Intent.ACTION_BATTERY_LOW)
+            addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+        }
+
+        registerReceiver(receiver, intentFilter )
+
+    }
+
+
+    override fun onDestroy() {
+
+        super.onDestroy()
+    }
+}
---
+++ app/src/main/java/com/ulyanenko/broadcast/MyReceiver.kt
@@ -0,0 +1,22 @@
+package com.ulyanenko.broadcast
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class MyReceiver: BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+        when (intent?.action){
+            Intent.ACTION_BATTERY_LOW -> {
+                Toast.makeText(context, "Battery is low", Toast.LENGTH_SHORT).show()
+            }
+            Intent.ACTION_AIRPLANE_MODE_CHANGED->{
+                Toast.makeText(context, "Airplane mode changed", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+}
